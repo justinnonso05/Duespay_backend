ccbd32afcdc62db754d0f75428d39ac71de87e48
justinnonso05 <chinonsoali2005@gmail.com>
2025-06-12 21:38:48 +0100

    emails

--- Duespay_backend/settings.py
+++ Duespay_backend/settings.py
@@ -186,4 +186,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- main/signals.py
+++ main/signals.py
@@ -1,6 +1,8 @@
+from django.db.models.signals import pre_save
+from .emails import send_admin_new_transaction_email, send_payer_transaction_verified_email
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import AdminUser, Association, ReceiverBankAccount
+from .models import AdminUser, Association, ReceiverBankAccount, Transaction
 
 @receiver(post_save, sender=AdminUser)
 def create_association_for_user(sender, instance, created, **kwargs):
@@ -17,12 +19,26 @@ def create_association_for_user(sender, instance, created, **kwargs):
             association_short_name=short_name,
         )
 
-# @receiver(post_save, sender=Association)
-# def create_bank_account_for_association(sender, instance, created, **kwargs):
-#     if created:
-#         ReceiverBankAccount.objects.create(
-#             association=instance,
-#             bank_name="--",
-#             account_name="--",
-#             account_number="--"
-#         )
+
+@receiver(post_save, sender=Transaction)
+def notify_admin_on_transaction(sender, instance, created, **kwargs):
+    if created:
+        association = instance.association
+        admin = association.admin
+        if admin.email:
+            send_admin_new_transaction_email(admin, association, instance)
+
+
+
+@receiver(pre_save, sender=Transaction)
+def send_verification_email_to_payer(sender, instance, **kwargs):
+    if not instance.pk:
+        # New transaction, not an update
+        return
+    try:
+        old_instance = Transaction.objects.get(pk=instance.pk)
+    except Transaction.DoesNotExist:
+        return
+    # Check if is_verified changed from False to True
+    if not old_instance.is_verified and instance.is_verified:
+        send_payer_transaction_verified_email(instance.payer, instance)
