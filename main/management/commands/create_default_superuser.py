a8bdb1ab46f3d7457fd63eed4e5469383e9476a5
justinnonso05 <chinonsoali2005@gmail.com>
2025-06-22 00:25:41 +0100

    superuser

---
+++ main/management/commands/create_default_superuser.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("justondev", "justondev05@gmail.com", "nonso2005")
+            self.stdout.write(">>>> Superuser created.")
+        else:
+            self.stdout.write("!!!! Superuser already exists.")
