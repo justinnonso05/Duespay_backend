467da438e03f225a9feceaec82eb4b07b395b7e0
justinnonso05 <chinonsoali2005@gmail.com>
2025-11-21 16:51:10 +0100

    remove migrate

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate --noinput
+# python manage.py migrate --noinput
 # python manage.py create_default_superuser
